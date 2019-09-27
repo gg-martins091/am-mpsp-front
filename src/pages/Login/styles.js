c34ad61691eefe4122aa72a5af0291dc3718da94
ggmartins9 <gabriel.gmartins091@gmail.com>
2019-09-27 20:57:00 -0300

    First Commit

---
+++ src/pages/Login/styles.js
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+export const Form = styled.form`
+  width: 400px;
+  background: #fff;
+  padding: 30px 25px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 10px;
+  img {
+    margin-bottom: 25px;
+  }
+  p {
+    color: #ff3333;
+    margin-bottom: 15px;
+    border: 1px solid #ff3333;
+    padding: 10px;
+    width: 100%;
+    text-align: center;
+  }
+  input {
+    flex: 1;
+    margin-bottom: 15px;
+    padding: 10px 20px;
+    color: #777;
+    font-size: 15px;
+    width: 100%;
+    border: 1px solid #ddd;
+    &::placeholder {
+      color: #999;
+    }
+  }
+  button {
+    color: #fff;
+    font-size: 16px;
+    background: #6700b5;
+    height: 56px;
+    border: 0;
+    border-radius: 5px;
+    width: 40%;
+    margin-top: 10px;
+    cursor: pointer;
+  }
+  hr {
+    margin: 20px 0;
+    border: none;
+    border-bottom: 1px solid #cdcdcd;
+    width: 100%;
+  }
+  a {
+    font-size: 16;
+    font-weight: bold;
+    color: #999;
+    text-decoration: none;
+  }
+`;
