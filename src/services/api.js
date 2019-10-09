671efd811532eb57e7ef1faed13a9ec356ecc740
Gabriel Martins <gabriel.martins@brtk.com.br>
2019-10-09 13:19:00 -0300

    Nova api url

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: "https://am-mpsp.appspot.com/api/v1"
+  baseURL: "http://104.154.104.125/api/v1"
 });
 
 api.interceptors.request.use(async config => {
